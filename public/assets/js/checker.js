5bb8b79e840b44fb303c8bed14cf39d386bffaba
Иван Чучков <yutub.lol@yandex.ru>
2024-11-28 23:51:04 +0300

    убрал маг с модалкой!!!

--- public/assets/js/checker.js
+++ public/assets/js/checker.js
@@ -1,43 +1,47 @@
 // Функция для проверки авторизации пользователя
 async function checkUserAuth() {
-    try {
-        const response = await fetch('/api/user', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
+  try {
+    const response = await fetch("/api/user", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-        console.log('Response status:', response.status); // Логируем статус ответа
+    console.log("Response status:", response.status); // Логируем статус ответа
 
-        if (response.status === 401) {
-            console.log("User is not authorized (401)");
-            updateAuthButton('Войти', '/login.html');
-        } else if (response.ok) {
-            const user = await response.json();
-            console.log('User data:', user); // Логируем данные пользователя
-            updateAuthButton('Профиль', './profile.html');
-        } else {
-            console.error('Error checking authorization:', response.status);
-        }
-    } catch (error) {
-        console.error('Error checking user authorization:', error);
-        updateAuthButton('Войти', '/login.html');
+    if (response.status === 401) {
+      console.log("User is not authorized (401)");
+      updateAuthButton("Войти", "/login.html");
+    } else if (response.ok) {
+      const user = await response.json();
+      console.log("User data:", user); // Логируем данные пользователя
+      if (user.role == "organization") {
+        updateAuthButton("Профиль", "./profileOrg.html");
+      } else if (user.role == "student") {
+        updateAuthButton("Профиль", "./profile.html");
+      } else {
+        updateAuthButton("Админ панель", "./admin.html");
+      }
+    } else {
+      console.error("Error checking authorization:", response.status);
     }
+  } catch (error) {
+    console.error("Error checking user authorization:", error);
+    updateAuthButton("Войти", "/login.html");
+  }
 }
-
 
 // Функция для обновления кнопки в зависимости от статуса авторизации
 function updateAuthButton(buttonText, buttonLink) {
-    const button = document.getElementById('logOrProfBut');
-    if (!button) {
-        console.error('Button with id "logOrProfBut" not found');
-        return;
-    }
-    button.innerHTML = `<b>${buttonText}</b>`; // Меняем текст
-    button.setAttribute('href', buttonLink); // Меняем ссылку
+  const button = document.getElementById("logOrProfBut");
+  if (!button) {
+    console.error('Button with id "logOrProfBut" not found');
+    return;
+  }
+  button.innerHTML = `<b>${buttonText}</b>`; // Меняем текст
+  button.setAttribute("href", buttonLink); // Меняем ссылку
 }
 
-
 // Проверяем авторизацию при загрузке страницы
-document.addEventListener('DOMContentLoaded', checkUserAuth);
+document.addEventListener("DOMContentLoaded", checkUserAuth);
