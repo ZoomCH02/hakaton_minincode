c9c6c19b0f92e4db7da785452ac287505a73a113
ZoomCH02 <you@example.com>
2024-11-28 10:27:45 +0300

    батник для запуска сервера и его хостинга

---
+++ start_server.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Генерация SSH-ключей
+ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ""
+
+REM Установка SSH-туннеля
+start /B ssh -R 80:127.0.0.1:3000 localhost.run
+
+REM Запуск сервера с помощью npm
+start /B npm run start
+
+REM Ожидание завершения процессов
+wait
